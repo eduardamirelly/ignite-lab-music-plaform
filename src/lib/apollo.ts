6aa9608f6b65e235b105616babf758b2f3e3867b
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-08-10 08:45:00 -0300

    :wrench: chore: .env.example

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/cl6mdvrlf0g4101uge7jtfs1i/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 });
