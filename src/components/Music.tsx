36969017d3973994c598cb8018e5a9390dfc0a57
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-08-09 21:08:47 -0300

    :lipstick: feat: Sidebar and Music component

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,3 +1,40 @@
-export function Music() {
-  return <h1>Music</h1>;
+import { CheckCircle, Lock } from 'phosphor-react';
+
+interface MusicProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'play' | 'playing';
+}
+
+export function Music(props: MusicProps) {
+  const isMusicAvailable = false;
+
+  return (
+    <a href="#">
+      <span className="text-gray-300">{props.availableAt.toString()}</span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          {isMusicAvailable ? (
+            <span className="text-sm text-brown-400 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Vídeo liberado
+            </span>
+          ) : (
+            <span className="text-sm text-warning-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+
+          <span className="text-xs uppercase rounded px-2 py-[0.125rem] text-white border border-brown-500 font-bold">
+            {props.type === 'play' ? 'Tocar' : 'Tocando'}
+          </span>
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+      </div>
+    </a>
+  );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,20 @@
+import { Music } from './Music';
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>;
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Playlist de Músicas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        <Music
+          title="Música #00"
+          slug="musica-00"
+          availableAt={new Date()}
+          type="play"
+        />
+      </div>
+    </aside>
+  );
 }
